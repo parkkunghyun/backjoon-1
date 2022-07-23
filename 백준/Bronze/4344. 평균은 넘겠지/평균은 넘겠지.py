f1a3e53c621881d55050bb025aa60fc76d7f50a1
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2022-07-23 21:20:49 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    nums = list(map(int, input().split()))
+    avg = sum(nums[1:])/nums[0]
+    cnt = 0
+    for i in nums[1:]:
+        if i > avg:
+            cnt +=1
+    rate = cnt/nums[0] *100
+    print(f'{rate:.3f}%')
