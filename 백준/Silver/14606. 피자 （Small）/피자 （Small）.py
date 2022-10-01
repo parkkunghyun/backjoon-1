449ce06298eb074c4aab833d4b26950ba9a3bc10
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2022-10-01 18:06:34 +0900

    [Silver V] Title: 피자 (Small), Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+
+if n ==1:
+    print(0)
+elif n==2:
+    print(1)
+elif n==3:
+    print(3)
+elif n==4:
+    print(6)
+elif n==5:
+    print(10)
+elif n==6:
+    print(15)
+elif n==7:
+    print(21)
+elif n==8:
+    print(28)
+elif n==9:
+    print(36)
+elif n==10:
+    print(45)
+
