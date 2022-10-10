54750cdb8a5605eab2c1af916e8cf9f73fb5b991
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2022-10-10 10:16:52 +0900

    [Silver IV] Title: 가로수, Time: 140 ms, Memory: 38548 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def gcd_func(a, b):
+    while b != 0:
+        a, b = b, a % b
+
+    return a
+
+
+input = sys.stdin.readline
+N = int(input())
+
+trees = [int(input()) for _ in range(N)]
+
+gaps = []
+for i in range(1, N):  # 가로수의 간격
+    gaps.append(trees[i] - trees[i - 1])
+
+gaps_set = list(set(gaps))  # 간격 중복 제거
+
+gcd = gaps_set[0]
+for i in range(1, len(gaps_set)):  # 간격들의 최대공약수
+    gcd = gcd_func(gcd, gaps_set[i])
+
+answer_tree_cnt = 0
+for gap in gaps:  # 간격을 최대공약수로 나누고 1을 빼주면 심을 나무 숫자
+    answer_tree_cnt += gap // gcd - 1
+
+print(answer_tree_cnt)
