9bb348bb10580d5e255d672a6a56efb63c8f93a7
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2022-12-07 15:38:57 +0900

    [Silver III] Title: 안테나, Time: 220 ms, Memory: 52152 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.py
+++ 백준/Silver/18310. 안테나/안테나.py
@@ -7,21 +7,15 @@ n = int(input())
 ans = list(map(int, input().split()))
 ans.sort()
 
-if len(ans) % 2 == 0:
-  mid = len(ans) // 2
-  midMinus = len(ans) // 2 - 1
-  count1 = 0
-  count2 = 0
-  for i in range(len(ans)):
-    count1 += abs(ans[i] - ans[mid])
-    count2 += abs(ans[i] - ans[midMinus])
-  if count1 < count2:
-    print(ans[mid])
-  else:
-    print(ans[midMinus])
-else:
-  mid = len(ans) // 2
-  count1 = 0
-  for i in range(len(ans)):
-    count1 += abs(ans[i] - ans[mid])
+mid = len(ans) // 2
+midMinus = len(ans) // 2 - 1
+count1 = 0
+count2 = 0
+for i in range(len(ans)):
+  count1 += abs(ans[i] - ans[mid])
+  count2 += abs(ans[i] - ans[midMinus])
+
+if count1 < count2:
   print(ans[mid])
+else:
+  print(ans[midMinus])
