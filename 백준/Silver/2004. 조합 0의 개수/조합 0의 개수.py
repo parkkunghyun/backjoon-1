983ea0945820546efd92f2f7049d2a26519f8dda
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2023-01-12 22:41:19 +0900

    [Silver II] Title: 조합 0의 개수, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+def count2(N):
+    if N < 2:
+        return 0
+    
+    count = 0
+    while N >= 2:
+        count += N//2
+        N = N//2
+    return count
+
+def count5(N):
+    if N < 5:
+        return 0
+    
+    count = 0
+    while N >= 5:
+        count += N//5
+        N //= 5
+    return count
+
+two_count = count2(n) - count2(n-m) - count2(m)
+five_count = count5(n) - count5(n-m) - count5(m)
+print(min(two_count, five_count))
