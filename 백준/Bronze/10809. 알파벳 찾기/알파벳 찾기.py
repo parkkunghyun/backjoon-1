2f3d680562a907322f8a395659f5feb1170413b5
parkkunghyun <79096673+parkkunghyun@users.noreply.github.com>
2023-01-14 21:35:04 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,10 +1,5 @@
-s= input()
-check = [-1]* 26
-for i in range(len(s)):
-    if check[ord(s[i])-97] != -1:
-        continue
-    else:
-        check[ord(s[i])-97] = i 
-        
-for i in range(26):
-    print(check[i], end=' ')
+word = input()
+alphabet = list(range(97,123))  # 아스키코드 숫자 범위
+
+for x in alphabet :
+    print(word.find(chr(x))) 
